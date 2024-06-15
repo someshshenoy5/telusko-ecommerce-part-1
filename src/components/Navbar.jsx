ad5fd0670da4b8b4cd01cb7f0e7d4c787d80b01d
someshshenoy5 <shenoysomesh5@gmail.com>
2024-06-15 13:03:29 +0530

    Final Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
-import React,{useEffect} from "react";
+import React, { useEffect, useState } from "react";
+import Home from "./Home";
+import axios from "axios";
 
+const Navbar = ({ onSelectCategory, onSearch }) => {
+  const getInitialTheme = () => {
+    const storedTheme = localStorage.getItem("theme");
+    return storedTheme ? storedTheme : "light-theme";
+  };
 
-const Navbar = () => {
+  const [theme, setTheme] = useState(getInitialTheme());
+
+  const toggleTheme = () => {
+    const newTheme = theme === "dark-theme" ? "light-theme" : "dark-theme";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
     <>
       <header>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+        <nav className="navbar navbar-expand-lg fixed-top">
           <div className="container-fluid">
             <a className="navbar-brand" href="https://telusko.com/">
               Telusko
@@ -21,7 +39,10 @@ const Navbar = () => {
             >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
                   <a className="nav-link active" aria-current="page" href="/">
@@ -33,55 +54,46 @@ const Navbar = () => {
                     Add Product
                   </a>
                 </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Categories
-                  </a>
-                  <ul className="dropdown-menu">
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li><hr className="dropdown-divider"/></li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
+
+                {/* < className="nav-item dropdown"> */}
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="/"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Categories
+                </a>
+
+                <li className="nav-item"></li>
               </ul>
-              <div className="d-flex align-items-center">
-                <i className="bi bi-cart text-dark me-2">Cart</i>
-              <form className="d-flex" role="search">
+              <button className="theme-btn" onClick={() => toggleTheme()}>
+                {theme === "dark-theme" ? (
+                  <i className="bi bi-moon-fill"></i>
+                ) : (
+                  <i className="bi bi-sun-fill"></i>
+                )}
+              </button>
+              <div className="d-flex align-items-center cart">
+                {/* <a href="/cart" className="nav-link text-dark"> */}
+                <i
+                  className="bi bi-cart me-2"
+                  style={{ display: "flex", alignItems: "center" }}
+                >
+                  Cart
+                </i>
+                {/* </a> */}
+
                 <input
                   className="form-control me-2"
                   type="search"
                   placeholder="Search"
                   aria-label="Search"
                 />
-                <button className="btn btn-outline-success" type="submit">
-                  Search Products
-                </button>
-              </form>
-              <div/>
+                <div />
+              </div>
             </div>
-          </div>
           </div>
         </nav>
       </header>
